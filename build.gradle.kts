64f8db55e5a1e37ac19617e1dbb694d0e6f7951f
dev-jiwonyang <jiwonyang@devsisters.com>
2021-12-16 20:12:57 +0900

    feat: cockroachDB 연동

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation ("org.postgresql:postgresql:42.2.18")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/example/kopring/KopringApplication.kt
+++ src/main/kotlin/com/example/kopring/KopringApplication.kt
@@ -3,9 +3,26 @@ package com.example.kopring
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+import org.postgresql.ds.PGSimpleDataSource;
+
 @SpringBootApplication
 class KopringApplication
 
 fun main(args: Array<String>) {
 	runApplication<KopringApplication>(*args)
+
+	try {
+		val ds = PGSimpleDataSource()
+		ds.setServerNames(arrayOf("localhost"))
+		ds.setPortNumbers(intArrayOf(26257))
+		ds.setDatabaseName("defaultdb")
+		ds.setUser("root")
+		ds.setSsl(false)
+		ds.setSslMode("disable")
+		ds.setApplicationName("App")
+		println("Hey! You connected to your CockroachDB cluster.")
+	} catch (e: Exception) {
+		e.printStackTrace()
+	}
+
 }
